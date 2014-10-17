925d95f9b62465ee3474fe1f79a45df69ebdbc13
ronaldjosephdesmarais <ron.desmarais@gmail.com>
2014-10-17 10:21:25 -0700

    modified image dist page

--- openstack_dashboard/dashboards/project/images/image_dist/site_tables.py
+++ openstack_dashboard/dashboards/project/images/image_dist/site_tables.py
@@ -130,7 +130,7 @@ class DeleteCredential(tables.UpdateAction):
     def update(self, request, obj_id):
         print("delete Cred %s"%obj_id)
         result = glint.get_glint_url_and_token(request) 
-        data_json = requests.post("%sdeletecred/"%result['url'],data={"SITE_ID":obj_id,"USER_ID":request.user,"USER_TOKEN":"%s"%result['token'],"USER_TENANT":request.user.token.tenant['name']},cookies=None).text
+        data_json = requests.post("%sdeletecredential/"%result['url'],data={"SITE_ID":obj_id,"USER_ID":request.user,"USER_TOKEN":"%s"%result['token'],"USER_TENANT":request.user.token.tenant['name']},cookies=None).text
         #data_obj = json.loads(data_json)
         print "Received back %s"%data_json
 
