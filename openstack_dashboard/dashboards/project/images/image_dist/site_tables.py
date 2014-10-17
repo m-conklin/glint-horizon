5f15b8518d95a0f77a2ca452fab73d24a9602041
ronaldjosephdesmarais <ron.desmarais@gmail.com>
2014-10-16 17:31:59 -0700

    modified image dist page

--- openstack_dashboard/dashboards/project/images/image_dist/site_tables.py
+++ openstack_dashboard/dashboards/project/images/image_dist/site_tables.py
@@ -94,37 +94,9 @@ class AddCredential(tables.LinkAction):
         return True
     
 class DeleteCredential(tables.LinkAction):
-    data_type_singular = _("Credential")
-    data_type_plural = _("Credential")
-    policy_rules = (("credential", "delete_credential"),)
-
-    def allowed(self, request, site=None):
-        try:
-            # Protected images can not be deleted.
-            print("check if delete credential is allowed %s"%site)
-            #if site and site.protected:
-            #    return False
-            #if site:
-            #    return site.owner == request.user.tenant_id
-            # Return True to allow table-level bulk delete action to appear.
-            result = glint.get_glint_url_and_token(request) 
-            data_json = requests.post("%shascredential/"%result['url'],data={"CK_TYPE":"ONE","SITE_ID":site.id,"USER_ID":request.user,"USER_TOKEN":"%s"%result['token'],"USER_TENANT":request.user.token.tenant['name']},cookies=None).text
-            #print "Allow the cred button says %s"%data_json
-            data_dict = json.loads(data_json)
-            #print "Allow the cred button says %s"%data_dict
-            if data_dict['result'] is True:
-                #self.verbose_name = _("EditCredential")
-                return True
-            return False
-        except:
-            return True
-
-    def delete(self, request, obj_id):
-        print("delete Credential %s"%obj_id)
-        result = glint.get_glint_url_and_token(request) 
-        data_json = requests.post("%sdeletecredential/"%result['url'],data={"SITE_ID":obj_id,"USER_ID":request.user,"USER_TOKEN":"%s"%result['token'],"USER_TENANT":request.user.token.tenant['name']},cookies=None).text
-        #data_obj = json.loads(data_json)
-        print "Received back %s"%data_json
+    data_type_singular = _("Repository")
+    data_type_plural = _("Repository")
+    policy_rules = (("cred", "delete_cred"),)
 
     
 class DeleteSite(tables.DeleteAction):
